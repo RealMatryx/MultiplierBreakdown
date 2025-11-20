c22c5d828c16e8bdedf011dc79140eed22ada7a0
Supersonic Seven <b.rockholz@icloud.com>
2025-11-20 15:20:18 -0500

    Update game.js
    Yeah I found it

--- src/game.js
+++ src/game.js
@@ -405,7 +405,7 @@ export function getGameSpeedupFactor(effectsToConsider, blackHolesActiveOverride
 
   factor = factor.times(PelleUpgrade.timeSpeedMult.effectValue);
 
-  if (EndgameUpgrade(7).isBought) factor = Decimal.max(factor, player.records.endgame.peakGameSpeed);
+  if (EndgameUpgrade(7).isBought) factor = Decimal.max(factor, player.records.thisEndgame.peakGameSpeed);
 
   // 1e-300 is now possible with max inverted BH, going below it would be possible with
   // an effarig glyph.
