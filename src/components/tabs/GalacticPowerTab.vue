1cf9c2f991996c6c159ec933df23cbce5300dc0f
Supersonic Seven <b.rockholz@icloud.com>
2025-11-22 16:55:32 -0500

    Create GalacticPowerTab.vue
    First template

---
+++ src/components/tabs/GalacticPowerTab.vue
@@ -0,0 +1,46 @@
+<script>
+
+export default {
+  name: "GalacticPowerTab",
+  data() {
+    return {
+      nextPow: 0
+    };
+  },
+  computed: {
+    powers() {
+      return GalacticPowers.all;
+    },
+    nextAtDisplay() {
+      const first = this.nextPow?.id === 1;
+      const next = GalacticPower.nextPowUnlockGP;
+
+      if (first) return `The first Galactic Power unlocks at ${format(next)} Galactic Power.`;
+      return next === undefined
+        ? "All Galactic Powers unlocked"
+        : `Next Galactic Power unlocks at ${format(next)} Galactic Power.`;
+    }
+  },
+  methods: {
+    update() {
+      this.nextPow = GalacticPower.nextPow;
+    },
+    isPowerVisible() {
+      return GalacticPowers.id.isUnlocked;
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      Rows coming soon...
+    </div>
+    <div>{{ nextAtDisplay }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
