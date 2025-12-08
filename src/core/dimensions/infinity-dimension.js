ce6122dd8c66a3843196eeb142a3b8a9043402f8
Supersonic Seven <b.rockholz@icloud.com>
2025-12-08 15:44:15 -0500

    Update infinity-dimension.js
    Tesseract ID Compression Reduction

--- src/core/dimensions/infinity-dimension.js
+++ src/core/dimensions/infinity-dimension.js
@@ -340,7 +340,8 @@ export const InfinityDimensions = {
   },
 
   get compressionMagnitude() {
-    const reduction = Effects.product(EndgameMastery(82), EndgameUpgrade(2));
+    const extraReduction = ExpansionPack.enslavedPack.isBought ? Math.pow(1 / Math.log10(Tesseracts.effectiveCount), 0.2) : 1;
+    const reduction = Effects.product(EndgameMastery(82), EndgameUpgrade(2)) * extraReduction;
     return 10 * reduction;
   },
 
