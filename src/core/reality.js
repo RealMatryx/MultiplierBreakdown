abc0e169c7a75bce513567a5c398dd608741e303
Supersonic Seven <b.rockholz@icloud.com>
2025-12-10 13:38:24 -0500

    Update reality.js
    New Decimal

--- src/core/reality.js
+++ src/core/reality.js
@@ -345,7 +345,7 @@ function giveRealityRewards(realityProps) {
 
       // Encode iM values into the RM variable as e10000 * iM in order to only require one prop
       let machineRecord;
-      if (Currency.imaginaryMachines.value === 0) machineRecord = player.reality.maxRM;
+      if (Currency.imaginaryMachines.value.eq(0)) machineRecord = player.reality.maxRM;
       else machineRecord = DC.E10000.times(Currency.imaginaryMachines.value);
       player.celestials.teresa.lastRepeatedMachines = player.celestials.teresa.lastRepeatedMachines
         .clampMin(machineRecord);
