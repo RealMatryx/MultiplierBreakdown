2953c7db3f1ec42849835faeee110fe6725c39eb
Supersonic Seven <b.rockholz@icloud.com>
2025-12-11 17:28:37 -0500

    Update format.js
    Adding a new overflow formatting

--- src/core/format.js
+++ src/core/format.js
@@ -99,9 +99,14 @@ window.formatHybridLarge = function formatHybridLarge(value, places, placesUnder
   return `${formatInt(value)}`;
 };
 
+window.formatHybridFloat = function formatHybridFloat(value, digits) {
+  if (new Decimal(value).gt(1e12)) return `${format(value, digits, digits)}`;
+  return `${formatFloat(value, digits)}`;
+};
+
 window.formatDimboostParts = function formatDimboostParts(value, places, placesUnder1000) {
   return formatHybridLarge(value, 3, 0);
-}
+};
 
 window.timeDisplay = function timeDisplay(ms) {
   return TimeSpan.fromMilliseconds(ms).toString();
