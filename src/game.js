83c8ea614ba702d9da4fc3e3ffc4d54b8d22edf9
Supersonic Seven <b.rockholz@icloud.com>
2025-12-30 10:08:36 -0500

    Update game.js
    Finalized Infinities

--- src/game.js
+++ src/game.js
@@ -315,14 +315,16 @@ export function gainedInfinities() {
   }
   if (Pelle.isDoomed) {
     let pelleInfs = new Decimal(1);
-    if (PelleAchievementUpgrade.achievement87.isBought) pelleInfs = Effects.max(1, Achievement(87)).toDecimal();
+    if (PelleAchievementUpgrade.achievement87.isBought) pelleInfs = new Decimal(Effects.max(1, Achievement(87)));
     if (PelleDestructionUpgrade.timestudy32.isBought) pelleInfs = pelleInfs.timesEffectsOf(TimeStudy(32));
     if (PelleRealityUpgrade.boundlessAmplifier.isBought) pelleInfs = pelleInfs.timesEffectsOf(RealityUpgrade(5));
     if (PelleRealityUpgrade.innumerablyConstruct.isBought) pelleInfs = pelleInfs.timesEffectsOf(RealityUpgrade(7));
     if (PelleAchievementUpgrade.achievement131.isBought) pelleInfs = pelleInfs.timesEffectsOf(Achievement(131).effects.infinitiesGain);
     if (PelleDestructionUpgrade.timestudy191.isBought) pelleInfs = pelleInfs.timesEffectsOf(TimeStudy(191).effects.infinitiesGain);
     if (PelleAchievementUpgrade.achievement164.isBought) pelleInfs = pelleInfs.timesEffectsOf(Achievement(164));
-    //Leave this open for future Celestial effect applications
+    if (PelleCelestialUpgrade.raV3.isBought) pelleInfs = pelleInfs.timesEffectsOf(Ra.unlocks.continuousTTBoost.effects.infinity);
+    if (PelleDestructionUpgrade.destroyedGlyphEffects.isBought) pelleInfs = pelleInfs.times(getAdjustedGlyphEffect("infinityinfmult"));
+    if (PelleDestructionUpgrade.singularityMilestones.isBought) pelleInfs = pelleInfs.powEffectOf(SingularityMilestone.infinitiedPow);
     return pelleInfs;
   }
   let infGain = Effects.max(
