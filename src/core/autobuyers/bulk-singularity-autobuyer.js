7c72d5cdddb89dbf8d336724cc89d165120432c2
Supersonic Seven <b.rockholz@icloud.com>
2026-01-10 16:55:08 -0500

    Update bulk-singularity-autobuyer.js

--- src/core/autobuyers/bulk-singularity-autobuyer.js
+++ src/core/autobuyers/bulk-singularity-autobuyer.js
@@ -51,12 +51,12 @@ export class BulkSingularityAutobuyerState extends AutobuyerState {
 
   tick() {
     if (Singularity.timePerCondense.gt(this.upperBound) && this.data.hasUpperBound && player.celestials.laitela.singularityCapIncreases.gt(0)) {
-      const bulk = Math.floor(Decimal.log10(Singularity.timePerCondense.div(this.upperBound))) + 1;
+      const bulk = Decimal.floor(Decimal.log10(Singularity.timePerCondense.div(this.upperBound))).add(1);
       player.celestials.laitela.singularityCapIncreases = Decimal.max(player.celestials.laitela.singularityCapIncreases.sub(bulk), 0);
     }
 
     if (Singularity.timePerCondense.lt(this.lowerBound) && this.data.hasLowerBound) {
-      const bulk = Math.floor(Decimal.log10(Singularity.timePerCondense.div(this.lowerBound).recip())) + 1;
+      const bulk = Decimal.floor(Decimal.log10(Singularity.timePerCondense.div(this.lowerBound).recip())).add(1);
       player.celestials.laitela.singularityCapIncreases = player.celestials.laitela.singularityCapIncreases.add(bulk);
     }
   }
